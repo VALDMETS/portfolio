af6f07ca399394f8b2bcf716c0d028a1041d04dd
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-09-10 22:18:20 -0500

    creating an expanding system on click

--- app/scripts/projectitem.js
+++ app/scripts/projectitem.js
@@ -2,19 +2,30 @@ import React from 'react';
 import {browserHistory} from 'react-router';
 
 export default React.createClass({
+  getInitialState: function () {
+    return {
+      expand: false
+    }
+  },
   render: function() {
-    return ((
+    let expandedView;
+    if(this.state.expand) {
+      expandedView = <p>wow wee!</p>
+    }
+    return (
       <div className="expo" id={this.props.project.url_name} onClick={this.clickHandler}>
         <div className="parallel">
           <h3>{this.props.project.title}</h3>
           <p>{this.props.project.description}</p>
           <p className="date">{this.props.project.date}</p>
         </div>
+        {expandedView}
       </div>
-    ))
+    )
   },
   clickHandler: function () {
     console.log(this.props.project);
-    browserHistory.push(this.props.project.url_name);
+    // browserHistory.push(this.props.project.url_name);
+    this.setState({expand: !this.state.expand});
   }
 });
